dbff368b3161b3f6cf7a42dba333d4ca83c97789
Vane2005 <duran.vanessa@correounivalle.edu.co>
2024-09-28 17:03:25 -0500

    realice las funciones 'grande', 'complemento' e 'inclusion'

---
+++ app/bin/test/taller/CojuntosDifusosTest.scala
@@ -0,0 +1,31 @@
+package taller
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.junit.runner.RunWith
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ConjuntosDifusosTest extends AnyFunSuite {
+  val objConjuntosDifusos = new ConjuntosDifusos()
+
+    // Test funcion pertenece
+
+  test("Pertenece 10 en grande(2,2)") {
+    assert(objConjuntosDifusos.pertenece(10, objConjuntosDifusos.grande(2, 2)) == 0.6944444444444445)
+  }
+
+  test("Pertenece 10 en complemento(grande(2,2))") {
+    assert(objConjuntosDifusos.pertenece(10, objConjuntosDifusos.complemento(objConjuntosDifusos.grande(2, 2))) ==  0.30555555555555547)
+  }
+
+
+  // Test funcion inclusion
+
+  test("Inclusion grande(2,2) en grande(9,18)") {
+    assert(!objConjuntosDifusos.inclusion(objConjuntosDifusos.grande(2, 2), objConjuntosDifusos.grande(9, 18)))
+  }
+
+  test("Inclusion grande(2,2) en complemento(grande(2,2))") {
+    assert(!objConjuntosDifusos.inclusion(objConjuntosDifusos.grande(2, 2), objConjuntosDifusos.complemento(objConjuntosDifusos.grande(2, 2))))
+  }
+}
---
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -0,0 +1,52 @@
+package taller
+import scala.annotation.tailrec
+
+class ConjuntosDifusos {
+
+    type ConjDifuso = Int => Double
+    
+    def pertenece(elem: Int , s: ConjDifuso) : Double = {
+        s (elem)
+    }
+
+    def grande(d: Int , e: Int): ConjDifuso = {
+        def mma(n: Int): Double = {
+            math.pow((n.toDouble/(n+d).toDouble), e)
+        }
+        mma
+    }
+
+    def complemento(c: ConjDifuso) : ConjDifuso = {
+        def comp(n : Int) : Double ={
+            1 - c(n)
+        }
+        comp
+    }
+
+    def union(cd1: ConjDifuso , cd2: ConjDifuso) : Unit = {
+    // Implementaci´on de la funci´on union, debe retornar un ConjDifuso
+    }
+
+    def interseccion (cd1: ConjDifuso , cd2: ConjDifuso) : Unit = {
+    // Implementaci´on de la funci´on interseccion, debe retornar un ConjDifuso
+
+    }
+
+    def inclusion (cd1: ConjDifuso , cd2: ConjDifuso) : Boolean = {
+
+        @tailrec
+        def aux(n: Int): Boolean = {
+            if (n > 1000) true // Si llegamos al final del intervalo, es porque todos los elementos cumplen
+            else if (cd1(n) > cd2(n)) false // Si algún elemento no cumple, devolvemos false
+            else aux(n + 1) // Seguimos con el siguiente elemento
+        }
+
+        aux(0) // Comenzamos desde el elemento 0
+    }
+    
+
+    def igualdad(cd1: ConjDifuso , cd2: ConjDifuso) : Unit = {
+    // Implementaci´on de la funci´on igualdad, debe retornar un ConjDifuso
+    }
+
+}
